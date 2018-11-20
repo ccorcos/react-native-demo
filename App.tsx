64341086748bc87871512730276f763b311faf96
Chet Corcos <ccorcos@gmail.com>
2018-11-20 10:48:03 -0800

    Basic issue

--- App.tsx
+++ App.tsx
@@ -8,80 +8,107 @@
 
 import React, { Component } from "react"
 import {
-	Platform,
 	StyleSheet,
-	Text,
 	View,
-	KeyboardAvoidingView,
+	Platform,
+	Dimensions,
 	Keyboard,
 	LayoutAnimation,
-	Dimensions,
-	WebViewProperties,
 } from "react-native"
 import { WebView } from "react-native-webview"
-import KeyboardSpacer from "react-native-keyboard-spacer"
-
-const instructions = Platform.select({
-	ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-	android:
-		"Double tap R on your keyboard to reload,\n" +
-		"Shake or press menu button for dev menu",
-})
 
 interface AppProps {}
 
-interface AppState {}
+interface AppState {
+	height: number
+}
 
-const injectScript = `
-const event = new Event("deviceready")
-document.dispatchEvent(event)
-`
+const updateListener =
+	Platform.OS === "android" ? "keyboardDidShow" : "keyboardWillShow"
+const resetListener =
+	Platform.OS === "android" ? "keyboardDidHide" : "keyboardWillHide"
 
-// https://github.com/react-native-community/react-native-webview/blob/master/docs/Reference.md#automaticallyadjustcontentinsets
+const noAnimation = {
+	duration: 0,
+	// create: {
+	// 	type: LayoutAnimation.Types.linear,
+	// },
+	// update: {
+	// 	type: LayoutAnimation.Types.linear,
+	// },
+	// delete: {
+	// 	type: LayoutAnimation.Types.linear,
+	// },
+}
+
+const sprigAnimation = {
+	duration: 500,
+	create: {
+		duration: 300,
+		type: LayoutAnimation.Types.easeInEaseOut,
+		property: LayoutAnimation.Properties.opacity,
+	},
+	update: {
+		type: LayoutAnimation.Types.spring,
+		springDamping: 200,
+	},
+}
+
+const resizeAnimation = sprigAnimation
 
 export default class App extends Component<AppProps, AppState> {
-	webview = React.createRef<WebView>()
+	state = {
+		height: Dimensions.get("window").height,
+	}
 
-	onMessage = (e: any) => {
-		console.log("onMessage", e.nativeEvent.data)
-		if (this.webview.current) {
-			const webview = this.webview.current as any
-			console.log("postMessage", "pong")
-			webview.postMessage("pong")
-		}
+	componentDidMount() {
+		Keyboard.addListener(updateListener, this.updateKeyboardSpace)
+		Keyboard.addListener(resetListener, this.resetKeyboardSpace)
+	}
+
+	componentWillUnmount() {
+		Keyboard.removeListener(updateListener, this.updateKeyboardSpace)
+		Keyboard.removeListener(resetListener, this.resetKeyboardSpace)
+	}
+
+	updateKeyboardSpace = event => {
+		LayoutAnimation.configureNext(resizeAnimation)
+		this.setState({
+			height: event.endCoordinates.screenY,
+		})
+	}
+
+	resetKeyboardSpace = () => {
+		LayoutAnimation.configureNext(resizeAnimation)
+		this.setState({ height: Dimensions.get("window").height })
 	}
 
 	render() {
+		console.log("HEIGHT:", this.state.height)
 		return (
-			<View style={styles.view}>
+			<View style={{ flex: 1 }}>
 				<WebView
-					ref={this.webview}
+					style={{
+						flex: 1,
+						// The web view has a blue border around it.
+						// The html body has a red border arounf it.
+						borderColor: "blue",
+						borderStyle: "solid",
+						borderWidth: 4,
+						backgroundColor: " blue",
+					}}
 					automaticallyAdjustContentInsets={false}
 					overScrollMode="never"
 					bounces={false}
 					hideKeyboardAccessoryView={true}
-					// contentInset={{top: 10, left: 0, right: 0, bottom: 10}}
-					// allowsBackForwardNavigationGestures={true}
-					style={styles.webview}
-					source={{ uri: "http://localhost:3000/" }}
-					injectedJavaScript={injectScript}
-					userAgent={
-						"Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16B91 Cordova"
-					}
-					onMessage={this.onMessage}
+					source={{ uri: "http://localhost:8000/index.html" }}
 				/>
-				<KeyboardSpacer />
+				<View
+					style={{
+						height: Dimensions.get("window").height - this.state.height,
+					}}
+				/>
 			</View>
 		)
 	}
 }
-
-const styles = StyleSheet.create({
-	view: {
-		flex: 1,
-		backgroundColor: "white",
-	},
-	webview: {
-		flex: 1,
-	},
-})
---
+++ README.md
@@ -0,0 +1,7 @@
+
+```
+npm install
+python -m SimpleHTTPServer 8000
+npm start
+open ios/chetapp.xcodeproj
+```
